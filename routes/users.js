be2bcbec2019d048e8e4d01211f5b40f2466f1b1
nasrah01 <nasrah1@hotmail.co.uk>
2022-02-05 23:26:56 +0000

    adding user routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid'; 
 
 const router = express.Router();
 
+let users = [];
+
 router.get('/', (req, res) => {
-  res.send('User');
+  console.log(users)
+  res.send(users);
+})
+
+router.post('/', (req, res) => {
+  const user = req.body;
+
+  users.push({ ...user, id: uuidv4() });
+  res.send(`${user.firstName} has been added to the user list`)
+})
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  const userFound = users.find((user) => user.id === id);
+  res.send(userFound);
 })
 
 export default router;
