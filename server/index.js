cdd6ebc26751960564f146b2fff97701b35ac582
nasrah01 <nasrah1@hotmail.co.uk>
2022-02-09 21:46:08 +0000

    creating client

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv/config';
 import bodyParser from 'body-parser';
-import usersRoutes from "./routes/users.js";
+import usersRoutes from "./routes/users.js"
+import cors from "cors";
+
+
 
 const app = express();
 const PORT = process.env.PORT;
-
+app.use(cors());
 app.use(bodyParser.json());
 
-app.use("/users", usersRoutes);
+app.use("/", usersRoutes);
 
 app.get("/", (req, res) => {
   res.send("Homepage");
--- server/routes/users.js
+++ server/routes/users.js
@@ -5,28 +5,21 @@ const router = express.Router();
 
 let users = [];
 
+router.post("/api/users", (req, res) => {
+  console.log(req.body);
+
+  res.json({ message: "done" });
+
+  const user = req.body;
+
+  users.push({ ...user, id: uuidv4() });
+  res.send(`${user.name} has been added to the user list`);
+});
+
 router.get("/", (req, res) => {
   console.log(users);
   res.send(users);
 });
 
-router.post("/", (req, res) => {
-  const user = req.body;
-
-  users.push({ ...user, id: uuidv4() });
-  res.send(`${user.firstName} has been added to the user list`);
-});
-
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-
-  const userFound = users.find((user) => user.id === id);
-  res.send(userFound);
-});
-
-router.delete('/:id', (req, res) => {
-  const user = req.body;
-  res.send(`Delete ${user.firstName} from the system`)
-})
 
 export default router;
